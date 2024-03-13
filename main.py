3c0c4763c8c4ea1e5d962b9d4bb9b073426d455f
kdavid001 <127635293+kdavid001@users.noreply.github.com>
2024-03-13 20:40:18 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+tony = t.Turtle()
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+
+########### Challenge 5 - Spirograph ########
+
+
+tim.speed("fastest")
+tony.speed("fastest")
+
+def draw(size):
+    heading = 0
+    for _ in range(int(360/ size)):
+        color = random_color()
+        tony.color(color)
+        tony.circle(100)
+        tony.setheading(tim.heading()+ heading )
+        heading += 1
+
+draw(1)
+  
